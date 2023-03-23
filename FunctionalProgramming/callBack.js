eb22986e09f4f6d97611994107b0c4ef6ea6d728
Wd-ruhul <ruhul.wd@gmail.com>
2023-03-23 16:00:06 +0600

    add callback function

---
+++ FunctionalProgramming/callBack.js
@@ -0,0 +1,14 @@
+// In JavaScript, you can also pass a function as an argument to a function. This function that is passed as an argument inside of another function is called a callback function. For example,
+
+import js from js;
+function greet(name, cb) {
+  console.log('Hi ' + name);
+  cb();
+}
+
+function callme() {
+  console.log('callback');
+}
+
+greet('Ruhul Amin', callme);
+
